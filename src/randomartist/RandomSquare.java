3cfe16240bce5d7ca73f5fbaed81625293a90d2f
BenjaminPar <benjaminparment@Benjamins-MacBook-Pro.local>
2016-10-08 13:47:10 +0200

    square

---
+++ src/randomartist/RandomSquare.java
@@ -0,0 +1,29 @@
+package randomartist;
+
+import java.awt.Graphics;
+
+
+class RandomSquare extends RandomShape {
+    protected int lineLength;
+    protected boolean fill;
+    
+    
+    public randomSquare (int maxX, int maxY) {
+        super(maxX, maxY);
+        lineLength = random.nextInt(maxX);
+        fill = random.nextBoolean();
+    }
+    
+    void draw(Graphics g) {
+        g.setColor(color);
+        if(fill){
+            g.fillRect(x, y, maxX, maxY);
+        }
+        else{
+            g.drawRect(x, y, maxX, maxY);
+        }
+        
+    }
+    
+    
+}
